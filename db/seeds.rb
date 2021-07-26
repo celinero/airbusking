87a9c5e50bd75773f2a3dd15a9617d79a545c517
celine.roure <celine.roure.pro@gmail.com>
2021-07-26 23:00:18 +1000

    creates model genre

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+genres = [
+  {name: "Singer", description: "Folk, rock, pop, blues and jazz, R&B, hip hop, classical"},
+  {name: "Musician", description: "Rock, pop, blues and jazz, R&B, hip hop, classical"},
+  {name: "Dancer", description: "Hip hop, funk, breakdance, done by individual or by group"},
+  {name: "Magician", description: "Illusion by sleight of hand or deceptive devices"},
+  {name: "Mime", description: "Silent art that involves acting using only movements, gestures, and facial expressions"},
+  {name: "Puppeteer", description: "Entertainer manipulating a puppet"},
+  {name: "Living statue artist", description: "A character with costume and make up, that perform a statue and interact with his audience "},
+  {name: "Pavement art", description: "Chalk artist, painter"},
+  {name: "Street theatre", description: "Dramatization of social and political issues played outdoors"},
+  {name: "Comedians", description: "Stand up performers or Joke busker"},
+  {name: "Circus skills", description: "Fire manipulation, juggling, acrobatics etc..."}
+]
+
+if User.count == 0
+  User.create(username: "Tester", email: "test@test.com", password: "password", password_confirmation: "password")
+end
+
+
+if Genre.count == 0
+  genres.each do |genre|
+    Genre.create(name: genre[:name], description: genre[:description])
+    puts "Created #{genre[:name]} genre"
+  end
+end
