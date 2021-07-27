db2720dad3308eae3f1f1ae5d2616f7b37b72825
celine.roure <celine.roure.pro@gmail.com>
2021-07-27 23:10:39 +1000

    personalises alert message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def select_alert_subclass(input)
+    case input
+    when 'notice'
+      return 'info'
+    when 'succes'
+      return 'success'
+    when 'error'
+      return 'danger'
+    when 'alert'
+      return 'warning'
+    else
+      return 'info'
+    end
+  end
 end
