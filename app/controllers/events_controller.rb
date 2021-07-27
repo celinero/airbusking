ae7b6f76d43b23055ef9ce53ebbdb6c0c143b986
celine.roure <celine.roure.pro@gmail.com>
2021-07-27 23:09:29 +1000

    styles event pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = Event.all.includes(:genre)
   end
 
   # GET /events/1 or /events/1.json
